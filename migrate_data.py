f63daeaae352721f90167648a8703dd125b9c8e3
Austin Ogilvie <austinogilvie@gmail.com>
2014-05-12 13:14:45 -0400

    some logging

--- migrate_data.py
+++ migrate_data.py
@@ -15,8 +15,13 @@ db_old = MongoClient(MONGO_URI1)["crunchbase"]
 MONGO_URI2 = os.environ.get('MONGO_URI2', 'mongodb://localhost:27017/')
 db_new = MongoClient(MONGO_URI2)["yhat"]
 
+total = db_old['entities'].count()
+counter = 0
 for doc in db_old['entities'].find({}).batch_size(100):
-    exists = db_new['crunchbase'].find_one({"type": "entity", "data.permalink": doc["permalink"]}, {"_id": 1})
+    counter += 1
+    if counter % 10:
+        print "{} of {} done".format(counter, total)
+    exists = db_new['crunchbase'].find_one({"type": "entity", "data.permalink": doc["permalink"]}, {"_id": 1, {"data.entity_type": 1}})
     if not exists:
         doc.pop("_id")
         record = {
@@ -26,6 +31,5 @@ for doc in db_old['entities'].find({}).batch_size(100):
         db_new["crunchbase"].save(record)
     else:
         print "skipping {}".format(exists)
-    break
 
 
